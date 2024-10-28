ffa5e4b3d978e0f3ce1015a11db2f666e9534a77
sweidy <sarahweidman7@gmail.com>
2024-10-28 11:00:45 -0400

    adding conv state swap functions to corrector

--- src/control/runtime_opts.F90
+++ src/control/runtime_opts.F90
@@ -91,6 +91,7 @@ subroutine read_namelist(nlfilename, single_column, scmlat, scmlon)
    use tracers,             only: tracers_readnl
    use nudging,             only: nudging_readnl
    use corrector,           only: corrector_readnl
+   use conv_state_swap,     only: conv_state_swap_readnl
 
    use dyn_comp,            only: dyn_readnl
    use ionosphere_interface,only: ionosphere_readnl
@@ -188,6 +189,7 @@ subroutine read_namelist(nlfilename, single_column, scmlat, scmlon)
    call scam_readnl(nlfilename, single_column, scmlat, scmlon)
    call nudging_readnl(nlfilename)
    call corrector_readnl(nlfilename)
+   call conv_state_swap_readnl(nlfilename)
 
    call dyn_readnl(nlfilename)
    call ionosphere_readnl(nlfilename)
